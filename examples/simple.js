1cd4074e848f47e4e0fc472c4c7c2dcadbd2587e
firejoust <0gr8tnop@duck.com>
2021-12-13 16:01:24 +1100

    Examples: make the simple.js example even simpler

--- examples/simple.js
+++ examples/simple.js
@@ -1,36 +1,52 @@
 /*
-**  This is a basic example of how someone can make their bot shoot a target with a bow.
+**  A simple example of how a mineflayer bot can act as a turret (assuming a bow is actively being held)
+**  - Utilises the bot.projectile.getAngle function to determine an angle required to hit a target
+**  - References bot.projectile.types.bow to specify that the trajectory should be calculated for an arrow
 */
 
 const mineflayer = require(`mineflayer`);
 const projectile = require(`mineflayer-projectile`);
-const bot = mineflayer.createBot(); // etc. (https://github.com/PrismarineJS/mineflayer/blob/master/docs/api.md#mineflayercreatebotoptions)
+const bot = mineflayer.createBot({}); // etc.
+bot.loadPlugin(projectile);
 
-bot.once(`login`, login);
+// global variables
+let attack = false;
+let occupied = false;
 
-function login() {
-  bot.loadPlugin(projectile);
-  setTimeout(scan, 10 * 1000); // within 10 seconds of joining, find a new target to shoot
+// global functions
+const delay = async ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-async function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function scan() {
-  let target = bot.nearestEntity(entity => entity.name.toLowerCase() === `player`);
-  if (target) {
-    bot.chat("Sniping's a good job mate.");
-    await shoot(target);
-  }
-}
-
-async function shoot(target) {
+const shoot = async target => {
   bot.activateItem();
-  
-  // allow enough time to charge the bow (10 ticks)
+  // wait 10 ticks to charge a bow (1 tick is 50 milliseconds)
   await delay(1000);
+  // lock onto the target's position
   let angle = bot.projectile.getAngle(bot.projectile.types.bow, bot.entity.position, target.position);
   await bot.look(angle.x, angle.y);
+  // release bow
   bot.deactivateItem();
 }
+
+// listeners
+bot.on("message", json => {
+  let message = json.toString();
+  // the message "$attack" has been sent in the chat
+  if (message.includes("$attack")) {
+    attack = !attack;
+    bot.chat(attack ? "Now autonomously firing towards the closest enemy!" : "Ceasing all operation!");
+  }
+});
+
+bot.on("physicsTick", async () => {
+  // wait for existing operations to finish
+  if (attack && !occupied) {
+    occupied = true;
+    // determine an enemy to target (players only)
+    let target = bot.nearestEntity(entity => entity.username);
+    if (target) {
+      await shoot(target);
+    }
+    occupied = false;
+  }
+});
